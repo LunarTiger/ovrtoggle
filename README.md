01573da50c67fabd502f24e3d65ac28ae31f0e19
LunarTiger <lunartiger@live.com>
2018-09-07 17:54:04 -0400

    :tada:

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ovrtoggle
+
+This batch file lets you start and stop the Oculus VR service.
+
+It checks to make sure you're running as an admin before showing you the state of the service and confirming you want to start or stop it.
+
+[Download Batch File Here](https://lunartiger.github.io/ovrtoggle/ovrtoggle.bat)
+
+[Download Executable File Here](https://lunartiger.github.io/ovrtoggle/ovrtoggle.exe)
+
+[View File Here](https://raw.githubusercontent.com/LunarTiger/ovrtoggle/master/ovrtoggle.bat)
+
+<hr />
+
+Use `https://github.com/LunarTiger/ovrtoggle.git` to clone the repo
+
+[Download ZIP](https://github.com/LunarTiger/ovrtoggle/archive/master.zip)
+
+<hr />
+
+[My Website](https://lunartiger.github.io/)
---
+++ ovrtoggle.bat
@@ -0,0 +1,60 @@
+@echo off
+title Oculus VR Service Toggle
+color 02
+
+echo Administrative permissions required. Detecting permissions...
+net session >nul 2>&1
+if %errorlevel% == 0 (
+echo Success: Administrative permissions confirmed.
+echo --------------------------
+) else (
+color 0c
+echo Failure: Current permissions inadequate, please run as administrator.
+goto :end
+)
+
+sc config OVRService start= demand >nul
+sc query "OVRService" | find "RUNNING" >nul
+if %errorlevel% == 1 goto :startq
+if %errorlevel% == 0 goto :stopq
+goto :error
+
+:startq
+echo The Oculus VR Service is not running.
+echo Do you wish to start the Oculus VR Service?
+echo                                 [1 = Yes, start service] [2 = No, keep service stopped]
+set /P c=
+
+if /I "%c%" EQU "1" goto :start
+if /I "%c%" EQU "2" exit
+cls
+goto :startq
+
+:start
+net start "OVRService"
+goto :end
+
+:stopq
+echo The Oculus VR Service is running.
+echo Do you wish to stop the Oculus VR Service?
+echo                                 [1 = Yes, stop service] [2 = No, keep service running]
+set /P c=
+
+if /I "%c%" EQU "1" goto :stop
+if /I "%c%" EQU "2" exit
+cls
+goto :stopq
+
+:stop
+net stop "OVRService"
+goto :end
+
+:error
+color 0c
+echo Error: Unable to verify if the Oculus VR Service is running.
+goto :end
+
+:end
+echo Press any key to exit.
+pause >nul
+exit
