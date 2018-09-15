d2109ca5185804bd191c42a999701170f8422095
LunarTiger <lunartiger@live.com>
2018-09-15 12:30:33 -0400

    cleaned up code and such

--- ovrtoggle.bat
+++ ovrtoggle.bat
@@ -6,7 +6,7 @@ echo Administrative permissions required. Detecting permissions...
 net session >nul 2>&1
 if %errorlevel% == 0 (
 echo Success: Administrative permissions confirmed.
-echo --------------------------
+cls
 ) else (
 color 0c
 echo Failure: Current permissions inadequate, please run as administrator.
@@ -27,14 +27,13 @@ echo Do you wish to start the Oculus VR Service?
 echo                                 [1 = Yes, start service] [2 = No, keep service stopped]
 set /P c=
 
-if /I "%c%" EQU "1" goto :start
+if /I "%c%" EQU "1" (
+net start "OVRService"
+goto :end
+)
 if /I "%c%" EQU "2" exit
 cls
 goto :startq
-
-:start
-net start "OVRService"
-goto :end
 
 :stopq
 echo The Oculus VR Service is running.
@@ -42,14 +41,13 @@ echo Do you wish to stop the Oculus VR Service?
 echo                                 [1 = Yes, stop service] [2 = No, keep service running]
 set /P c=
 
-if /I "%c%" EQU "1" goto :stop
+if /I "%c%" EQU "1" (
+net stop "OVRService"
+goto :end
+)
 if /I "%c%" EQU "2" exit
 cls
 goto :stopq
-
-:stop
-net stop "OVRService"
-goto :end
 
 :error
 color 0c
